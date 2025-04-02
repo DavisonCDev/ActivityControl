55715d14f41b703931dcff9231543e98fe7424d2
Davison Campos <davison.campos1@gmail.com>
2025-04-02 15:18:47 -0300

    Atualização de comentarios

--- src/main/java/com/oksmart/activitycontrol/repository/ActivityRepository.java
+++ src/main/java/com/oksmart/activitycontrol/repository/ActivityRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, String> {
-    // Métodos personalizados de consulta podem ser definidos aqui, se necessário
+
 }
